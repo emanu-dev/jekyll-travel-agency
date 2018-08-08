92808fe376258d201ad0539e6f761c839ca8813d
Emanuel <front@entrecliques.com.br>
2018-08-08 18:28:10 -0300

    [UPDATE] Body overflow state

--- _javascript/app.js
+++ _javascript/app.js
@@ -8,6 +8,7 @@ const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
 mobileMenuToggle.addEventListener('click', ()=>{
 	mobileMenu.classList.toggle('--active');
 	mobileMenuToggle.classList.toggle('--active');
+	document.body.classList.toggle('--locked');
 });
 
 /*=====  End of Mobile Menu  ======*/
