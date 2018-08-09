2aab6988ae9e60899f4f15fb2b4404204a409dbc
Emanuel <front@entrecliques.com.br>
2018-08-09 17:13:52 -0300

    [ADD] Menu Scroll Functionality

--- _javascript/app.js
+++ _javascript/app.js
@@ -1,3 +1,30 @@
+/*=================================
+=            Polyfills            =
+=================================*/
+
+// Overwrites native 'firstElementChild' prototype.
+// Adds Document & DocumentFragment support for IE9 & Safari.
+;(function(constructor) {
+    if (constructor &&
+        constructor.prototype &&
+        constructor.prototype.firstElementChild == null) {
+        Object.defineProperty(constructor.prototype, 'firstElementChild', {
+            get: function() {
+                var node, nodes = this.childNodes, i = 0;
+                while (node = nodes[i++]) {
+                    if (node.nodeType === 1) {
+                        return node;
+                    }
+                }
+                return null;
+            }
+        });
+    }
+})(window.Node || window.Element);
+
+/*=====  End of Polyfills  ======*/
+
+
 //TODO: Clean Up
 /*===================================
 =            Mobile Menu            =
@@ -91,7 +118,20 @@ slider.addEventListener('mouseleave', () =>{
 =            Menu Navigation            =
 =======================================*/
 
+let menuItems = [].slice.call(document.querySelectorAll('.header__menu-item'));
 
+menuItems.forEach((item) => {
+	item.firstElementChild.addEventListener('click', (e) => {
+		e.stopPropagation();
+		e.preventDefault();
+		
+		scrollView(item.getAttribute('data-section'));
+	});
+});
+
+function scrollView(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
 
 /*=====  End of Menu Navigation  ======*/
 
