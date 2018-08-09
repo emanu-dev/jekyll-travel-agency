cc77f6f487cbeba1d832214919b91f1bd2b55623
Emanuel <front@entrecliques.com.br>
2018-08-09 15:06:26 -0300

    [ADD] Main Slider Functionality

--- _javascript/app.js
+++ _javascript/app.js
@@ -1,3 +1,4 @@
+//TODO: Clean Up
 /*===================================
 =            Mobile Menu            =
 ===================================*/
@@ -12,3 +13,75 @@ mobileMenuToggle.addEventListener('click', ()=>{
 });
 
 /*=====  End of Mobile Menu  ======*/
+
+//TODO: Clean Up
+/*==============================
+=            Slider            =
+==============================*/
+
+const slider = document.getElementById('main-slider');
+const slides = [].slice.call(slider.querySelectorAll('.slider__list-item'));
+const slideQty = parseInt(slides.length);
+const slideNav = document.getElementById('slider-nav');
+
+let activeSlide = 0;
+let interval = null;
+
+slides.forEach(()=>{
+	slideNav.insertAdjacentHTML('beforeend','<div class=\'slider__nav-item\'></div>');
+	slideNav.firstElementChild.classList.add('--active');
+	slideNav.lastChild.addEventListener('click', (e) => {
+		let allItems = [].slice.call(slideNav.childNodes);
+		
+		allItems.forEach((item, index)=>{
+			if (item == e.currentTarget) {
+				item.classList.add('--active');
+				activeSlide = index;
+			} else {
+				item.classList.remove('--active');
+			}
+		});
+
+		slider.firstElementChild.style.left = ((activeSlide * 100) * -1).toString() + 'vw';
+	});
+});
+
+
+function slideAnimation() {
+	let allItems = [].slice.call(slideNav.childNodes);
+	activeSlide < allItems.length - 1 ? activeSlide += 1 : activeSlide = 0;
+	
+	let currentSlide = allItems[activeSlide];
+
+	allItems.forEach((item)=>{
+		if (item == currentSlide) {
+			item.classList.add('--active');
+		} else {
+			item.classList.remove('--active');
+		}
+	});
+
+	slider.firstElementChild.style.left = ((activeSlide * 100) * -1).toString() + 'vw';	
+}
+
+interval = setInterval(() => {
+	slideAnimation();
+}, 3000);
+
+
+slider.addEventListener('mouseover', () =>{
+	if (interval != null) {
+		clearInterval(interval);
+		interval = null;
+	}	
+});
+
+slider.addEventListener('mouseleave', () =>{
+	if (interval == null) {
+		interval = setInterval(() => {
+			slideAnimation();
+		}, 3000);
+	}	
+});
+
+/*=====  End of Slider  ======*/
