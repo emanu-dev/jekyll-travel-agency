a0add8a78e750b919b57de17eb7ca11bb1460b14
Emanuel <front@entrecliques.com.br>
2018-08-24 11:47:59 -0300

    [ADD] Full Page scroll on desktop

--- _javascript/modules/nav.menu.module.js
+++ _javascript/modules/nav.menu.module.js
@@ -4,19 +4,78 @@ const scrollView = (sectionId) => {
 
 const navMenu = () => {
 	let menuItems = [].slice.call(document.querySelectorAll('.header__menu-item'));
+	let currentSection = 0;
+	const sectionList = [].slice.call(document.getElementById('main').querySelectorAll('.sec'));
+	const min = 0;
+	const max = (sectionList.length - 1);
+	let canScroll = true;	
 
 	if (window.location.pathname.split( '/' ).length < 3) {
 
-		menuItems.forEach((item) => {
+		menuItems.forEach((item, index) => {
 			item.firstElementChild.addEventListener('click', (e) => {
 				e.stopPropagation();
 				e.preventDefault();
 				
 				scrollView(item.getAttribute('data-section'));
+				currentSection = index;
 				window.location.hash = `/${item.getAttribute('data-section')}`;
 			});
 		});
 	}
+
+	window.addEventListener('wheel', (e) => {
+	    e.preventDefault();
+	
+	    console.log(canScroll);
+
+	    if (canScroll) {
+	    	canScroll = false;
+		    if (e.deltaY < 0) {
+		        currentSection = Math.max(min, Math.min(currentSection - 1, max));
+		        
+		    }
+		    if (e.deltaY > 0) {
+		        currentSection = Math.max(min, Math.min(currentSection + 1, max));
+		    }
+
+		    setTimeout(()=>{
+		    	canScroll = true;
+		    	console.log(canScroll);
+		    }, 800);
+
+		    console.log(sectionList[currentSection].getAttribute('id'));
+		    window.location.hash = `/${sectionList[currentSection].getAttribute('id')}`;
+		    document.getElementById(sectionList[currentSection].getAttribute('id')).scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+
+
+	    }
+
+
+	});
+
+
+	window.addEventListener('touchmove', (e) => {
+	    e.preventDefault();
+	    var currentY = e.originalEvent.touches[0].clientY;
+	    if(currentY > lastY){
+	        console.log('scrolling up');
+	        console.log('Samuel L. Jackson says: Go down MOTHERFUCKER');
+	        currentSection = Math.max(min, Math.min(currentSection - 1, max));
+	    }else if(currentY < lastY){
+	        console.log('scrolling down');
+	        console.log('Samuel L. Jackson says: Go up MOTHERFUCKER');
+	        currentSection = Math.max(min, Math.min(currentSection + 1, max));
+	    }
+	    lastY = currentY;
+
+	    console.log(currentSection);
+	    window.location.hash = `/${sectionList[currentSection].getAttribute('id')}`;
+	    document.getElementById(sectionList[currentSection].getAttribute('id')).scrollIntoView({ behavior: 'smooth', block: 'start' });
+	});
+
+
 };
 
 export default navMenu;
